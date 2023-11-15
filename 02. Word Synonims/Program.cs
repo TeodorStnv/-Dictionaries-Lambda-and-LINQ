6bb11f0c1b28eb6769aef3a30e5d42d80bfa7a89
TeodorStnv <teodor.stnv@gmail.com>
2023-11-15 17:01:53 +0200

    Add project files.

---
+++ 02. Word Synonims/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+int num = int.Parse(Console.ReadLine());
+var ListOfSyn = new Dictionary<string, List<string>>();
+
+for (int i = 0; i < num; i++)
+{
+    string word = Console.ReadLine();
+    string synonym = Console.ReadLine();
+
+    if(!ListOfSyn.ContainsKey(word))
+    {
+        ListOfSyn.Add(word, new List<string> {synonym}); 
+    }
+    else
+    {
+        ListOfSyn[word].Add(synonym);
+    }
+}
+foreach (var word in ListOfSyn)
+{
+    Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+}
---
+++ 1. Count Real Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿List<double> numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(double.Parse)
+    .ToList();
+
+var dictionary = new SortedDictionary<double, double>();
+
+foreach(var number in numbers)
+{
+
+    if (dictionary.ContainsKey(number))
+    {
+        dictionary[number]++;
+     
+    }
+    else 
+    {
+        dictionary[number] = 1;
+    }
+  }
+foreach(var number in dictionary)
+{
+    Console.WriteLine($"{number.Key} -> {number.Value}");
+}
